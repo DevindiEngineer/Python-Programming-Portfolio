ee5f748ac34bc04f5a3adec83af1be3758ec1a9b
Devindi Wickramasinghe <devindicampus@gmail.com>
2026-08-16 13:35:12 +0530

    Add files via upload

---
+++ Python-Programming---Day-01-main/python files/Python_Day_01.py
@@ -0,0 +1,108 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "ea2c0be6-bca1-4a20-a728-10ce24def597",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello Devindi\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Hello Devindi\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8d08a2f2-5f65-428c-8e0c-8adced083adc",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello Sri Lanka\n",
+      "Welcome!\n",
+      "Have a nice vacation...\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Hello Sri Lanka\")\n",
+    "print(\"Welcome!\")\n",
+    "print(\"Have a nice vacation...\")\n",
+    "print(\"Im 20 yrs old\")"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "f1d474cf-448f-4c62-8581-f2e369e043ff",
+   "metadata": {},
+   "source": [
+    "# Hi this is a note\n",
+    "## Hi this is a note \n",
+    "### Hi this is a note"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 11,
+   "id": "716b3f04-997b-4cc8-b5b6-e45e8163c088",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "My name is Devindi Wickramasinghe,\n",
+      "I am a student at University of Sri Jayewardenepura,\n",
+      "I am learning python programming,\n",
+      "I am currently doing my degree.\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"My name is Devindi Wickramasinghe,\")\n",
+    "print(\"I am a student at University of Sri Jayewardenepura,\")\n",
+    "print(\"I am learning python programming,\")\n",
+    "print(\"I am currently doing my degree.\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "44f91ab2-a769-4585-a3c9-60d37e19cee0",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.14.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ Python-Programming---Day-01-main/python files/Untitled1.py
@@ -0,0 +1,77 @@
+{
+  "metadata": {
+    "kernelspec": {
+      "name": "xpython",
+      "display_name": "Python 3.13 (XPython)",
+      "language": "python"
+    },
+    "language_info": {
+      "codemirror_mode": {
+        "name": "ipython",
+        "version": 3
+      },
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "nbconvert_exporter": "python",
+      "pygments_lexer": "ipython3",
+      "version": "3.13.1"
+    }
+  },
+  "nbformat_minor": 5,
+  "nbformat": 4,
+  "cells": [
+    {
+      "id": "b1881bc0-fc46-4d3b-9ed5-2bcb571882cc",
+      "cell_type": "code",
+      "source": "import keyword",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [],
+      "execution_count": 1
+    },
+    {
+      "id": "5a0b0117-9f62-466e-95fd-5bde48acc294",
+      "cell_type": "code",
+      "source": "print(keyword.kwlist)",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [
+        {
+          "name": "stdout",
+          "output_type": "stream",
+          "text": "['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']\n"
+        }
+      ],
+      "execution_count": 2
+    },
+    {
+      "id": "3fbb2cc3-6fb5-4897-88b5-058fe758df55",
+      "cell_type": "code",
+      "source": "age=\"25\"",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [],
+      "execution_count": 3
+    },
+    {
+      "id": "e3e59233-0768-4467-862c-f8a22ce27c57",
+      "cell_type": "code",
+      "source": "print(age)",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [
+        {
+          "name": "stdout",
+          "output_type": "stream",
+          "text": "25\n"
+        }
+      ],
+      "execution_count": 4
+    }
+  ]
+}
